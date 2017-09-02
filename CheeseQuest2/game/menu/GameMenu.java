245a0c3fc3da2f3fc94e50ca452b366e88a74182
Inkstone <Inkstone@MacBook-Inkstone.local>
2017-09-01 21:49:38 -0600

    Start GameMenu outputLook()

--- CheeseQuest2/game/menu/GameMenu.java
+++ CheeseQuest2/game/menu/GameMenu.java
@@ -1,8 +1,12 @@
 package game.menu;
 
+import org.apache.commons.lang.WordUtils;
 import game.system.*;
 import game.object.room.*;
 
+/**
+ * Manages all in game actions
+ */
 public class GameMenu extends Menu {
 
     private static GameMenu instance;
@@ -29,6 +33,16 @@ public class GameMenu extends Menu {
     }
     public void quit() {
         changeToLoadMenu();
+    }
+
+    /**
+     * Look at room surroundings
+     * People present
+     * Items
+     * Adjacent rooms
+     */
+    public void outputLook() {
+        outputTitle(room.getSingleName());
     }
     // The Northmen say the world will come to an end. The sun will grow black. The earth will sink into the sea. The stars will disappear. Fire and water will meet. Flames will play against the sky. The heavens and earth and all the world will be burned. All the gods will be dead, and the warriors of Valhalla, and people everywhere. Senua, prepare yourself for Ragnarok, for it is nigh.
 }
